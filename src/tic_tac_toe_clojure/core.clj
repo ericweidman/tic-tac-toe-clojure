d6207825403782ef2508c8733372a3802567e8de
ericweidman <ericweidman@gmail.com>
2016-03-24 10:28:39 -0400

    Solved

--- src/tic_tac_toe_clojure/core.clj
+++ src/tic_tac_toe_clojure/core.clj
@@ -1,5 +1,30 @@
 (ns tic-tac-toe-clojure.core
   (:gen-class))
 
+
+(def board [[:e :x :e]
+            [:o :o :o]
+            [:x :e :x]])
+
+(defn solve [x]
+  (let [[[a b c]
+         [d e f] 
+         [g h i]] x
+        solutions (hash-set
+                     [a b c]
+                     [d e f]
+                     [g h i]
+                     [a d g]
+                     [b e h]
+                     [c f i]
+                     [a e i]
+                     [c e g])]
+    
+    (cond
+      (contains? solutions [:x :x :x]) :x
+      (contains? solutions [:o :o :o]) :o
+      :else nil)))
+      
+
 (defn -main []
-  (println "Hello, World!!"))
+  (println "Hello, World!"))
